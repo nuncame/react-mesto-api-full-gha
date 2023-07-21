0afca6fbee6d4ce4862ac183423d599c5bd2a09c
Victoria Markova <vii.nuncame@gmail.com>
2023-07-22 00:11:13 +0300

    Revert "fix: port"
    This reverts commit 79b39ef78703447db0f887a44cb833483cc1fcf5.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const NotFoundError = require('./errors/not-found-err');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const allowedCors = [
   'https://mesto-nuncame.nomoredomains.xyz',
